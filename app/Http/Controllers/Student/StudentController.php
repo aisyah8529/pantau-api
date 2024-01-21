d28da7da637e34d2bed96df7629e498784e75f7c
Abd Qayyum <ayum9422@gmail.com>
2024-01-21 13:46:02 +0800

    enable auth

--- app/Http/Controllers/Student/StudentController.php
+++ app/Http/Controllers/Student/StudentController.php
@@ -20,7 +20,7 @@ class StudentController extends Controller
 {
     public function __construct()
     {
-        // $this->middleware('auth:api');
+        $this->middleware('auth:api');
     }
 
     public function index(Request $request)
